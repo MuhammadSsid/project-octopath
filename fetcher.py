1d5a3d34b79dbce9b98f15452b0252603d48675c
Darkness <gitmastr@gmail.com>
2018-02-07 02:31:39 +0000

    Added in print to seed file function

--- fetcher.py
+++ fetcher.py
@@ -6,10 +6,10 @@ import re
 headers = {}
 headers['User-Agent'] = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
 
-#Request url using urllib
+#Request URL using urllib
 req = urllib.request.Request('http://wiprodigital.com', headers=headers)
 
-#
+#Read the requested URL
 html = urllib.request.urlopen(req).read()
 
 #Create a BeautifulSoup object using the "lxml" parser
@@ -17,5 +17,10 @@ soup = BeautifulSoup(html, "lxml")
 
 #This line just means find all the 'a' link tags and list them along with the urls
 for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-    print(link.get('href'))
+
+    #Print all links to a "seed" file that we can use later
+    for tag in link:
+        print(link.get('href'), file= open("seed.txt","a"))
+
+
 
